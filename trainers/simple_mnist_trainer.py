58180626769c432512d3bf01af77d340c02aeb17
Gideon Mendels <gideonm@gmail.com>
2018-04-10 23:28:14 -0400

    adding support for comet.ml

--- trainers/simple_mnist_trainer.py
+++ trainers/simple_mnist_trainer.py
@@ -25,6 +25,14 @@ class SimpleMnistModelTrainer(BaseTrain):
             )
         )
 
+        if hasattr(self.config,"comet_api_key"):
+            from comet_ml import Experiment
+            experiment = Experiment(api_key=self.config.comet_api_key, project_name=self.config.exp_name)
+            experiment.disable_mp()
+            experiment.log_multiple_params(self.config)
+
+            self.callbacks.append(experiment.get_keras_callback())
+
         self.callbacks.append(
             TensorBoard(
                 log_dir=self.config.tensorboard_log_dir,
