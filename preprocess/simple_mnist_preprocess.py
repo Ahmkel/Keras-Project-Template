8e21d8215ce424d7cb70251aac59d317636b4442
husseinjd <husseinjundi810@gmail.com>
2019-03-22 21:49:14 +0100

    simplemnist dummy preprocess

---
+++ preprocess/simple_mnist_preprocess.py
@@ -0,0 +1,9 @@
+#import import modules
+
+class SimpleMnistPreprocess(BasePreprocess):
+    def __init__(self,data):
+        super(BasePreprocess, self).__init__(data)
+
+    def preprocess(self):
+        #implement data preprocessing
+        pass
